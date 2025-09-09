4c1e0e417efda0059cd1433ef5ad088dd46139db
jaynik.s <188400672+Jaynik-S@users.noreply.github.com>
2025-09-09 12:15:21 -0400

    tested multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,13 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int result = multiples_of_three_and_five();
-        System.out.println("There are " + result + " multiples of 3 or 5 that are below 1000");
+        System.out.println("There are " + multiples(1000, 3, 5) + " multiples of 3 or 5 that are below 1000");
     }
 
-    private static int multiples_of_three_and_five() {
+    public static int multiples(int n, int a, int b) {
         int counter = 0;
-        for (int i = 1; i <= 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
                 counter++;
             }
         }
